35795d2e31a3f7e2ae47f9d7fa72da27aff5496d
ilosz01-ThinkPad <ilonaszczot@gmail.com>
2025-10-14 14:26:27 +0200

    Restructures folders

--- path_config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-local_path = os.getcwd()
-data_path = os.path.join(local_path, 'R_data')
-output_path = os.path.join(local_path, 'output')
-
---
+++ python_notebooks/path_config.py
@@ -0,0 +1,6 @@
+import os
+
+local_path = os.getcwd()
+data_path = os.path.join(local_path, 'R_data')
+output_path = os.path.join(local_path, 'output')
+
